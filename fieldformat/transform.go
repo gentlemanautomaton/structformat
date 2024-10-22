b0d69e1d11fdd4fa56175ec8137a27205d8040b3
Joshua Sjoding <ga@josh.hey.nu>
2024-10-21 23:38:30 -0700

    fieldformat: Added Transform.Options()

--- fieldformat/transform.go
+++ fieldformat/transform.go
@@ -3,9 +3,15 @@ package fieldformat
 // Transform is a function that transforms a field value.
 type Transform func(fieldValue string) string
 
-// Apply will set the field format inclusion for ref.
+// Apply will set the field format value transform for ref.
 func (t Transform) Apply(ref *Options) {
 	ref.Transform = t
+}
+
+// Options returns a set of field format options with the transform.
+func (t Transform) Options() (opts Options) {
+	t.Apply(&opts)
+	return
 }
 
 // Wrap returns a transformation that wraps non-empty field values with the
